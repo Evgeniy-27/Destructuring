75c29603d8c8766fa160a2b998b5071a90bae029
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-07-12 14:07:00 +0700

    add test

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import descript, { character } from '../index';
+
+test('Совпадение данных', () => {
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  const received = descript(character);
+
+  expect(received).toEqual(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,43 @@
+export const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+export default function descript(obj) {
+  const result = [];
+  for (const elem of obj.special) {
+    const {
+      id,
+      name,
+      icon,
+      description = 'Описание недоступно',
+    } = elem;
+    result.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  }
+  return result;
+}
+
+// console.log(descript(character))
